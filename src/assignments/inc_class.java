d16ecef811f0733357394a4c32959dfc74f62726
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-15 23:12:32 +0530

    This is my increment assignment.

---
+++ src/assignments/inc_class.java
@@ -0,0 +1,23 @@
+package assignments;
+
+public class inc_class {
+public static void main(String[] args) {
+	double db1 = 14.5d;
+
+	db1++;
+
+	System.out.println(db1);
+
+    int i = 10;
+
+	i++;
+
+	System.out.println(i);
+
+    boolean bool = true;
+
+	//bool++;
+
+	System.out.println(bool);
+}
+}
