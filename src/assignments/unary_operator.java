8adc72f257e09e4cc37e22de089aa3efb7f659e4
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-16 22:20:32 +0530

    Unary operator test

---
+++ src/assignments/unary_operator.java
@@ -0,0 +1,13 @@
+package assignments;
+
+public class unary_operator {
+	public static void main(String[] args) {
+		   int i = 10;   
+
+		   i = i++ + i + ++i ;
+		   
+
+		   System.out.println(i);
+	}
+
+}
