ba1f872a2aa56778989cc2be23d864576e776a7f
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-26 21:55:55 +0530

    Arithmetic operator solved 1

---
+++ src/assignments/Arithmetic_Operator1.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class Arithmetic_Operator1 {
+	public static void main(String[] args) {
+		int i = 10; 
+
+		int j = 7; 
+
+		System.out.println(i+j); 
+
+		System.out.println(i-j); 
+
+		System.out.println(i*j); 
+
+		System.out.println(i/j); 
+
+		System.out.println(i%j);
+	}
+
+}
