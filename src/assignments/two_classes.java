5ff56e3e2c6c5bda2b33969bdd3464b1615e2d6c
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-26 17:21:18 +0530

    Output of unary code

---
+++ src/assignments/two_classes.java
@@ -0,0 +1,18 @@
+package assignments;
+
+public class two_classes {
+	public static void main(String[] args) {
+
+		System.out.println("This is my first class");
+	}
+	 class second {
+		  public static void main(String[] args) {
+				System.out.println("This is my 2nd class");
+		}
+	 }
+	
+}
+
+
+
+
