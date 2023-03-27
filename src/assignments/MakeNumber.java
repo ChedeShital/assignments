978a358e60d72b6f32123a5fa6da2941a4a5d2a3
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-28 00:12:24 +0530

    Number making using available operators.

---
+++ src/assignments/MakeNumber.java
@@ -0,0 +1,12 @@
+package assignments;
+
+public class MakeNumber {
+	public static void main(String[] args) {
+		int i=1;
+		int j=2;
+		int k=6;
+		int l=7;
+		System.out.println(( (k-i)*j)+l);
+	}
+
+}
