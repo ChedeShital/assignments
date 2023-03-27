9f3192654ff231157b6765e51311756f90fec448
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-27 23:07:00 +0530

    Swapping with variable.

---
+++ src/assignments/Swaping.java
@@ -0,0 +1,22 @@
+package assignments;
+
+public class Swaping {
+	public static void main(String[] args) {
+	    int i = 10;
+
+	    int j = 7;
+	    int temp;
+	    temp = i;
+	    i  = j;
+	    j = temp;
+	    
+ 
+
+	    System.out.println(i);
+
+	    System.out.println(j);
+
+	    // Expected Result , i = 7  and j = 10  
+	}
+
+}
