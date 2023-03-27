1b5abfe1570f1aefb7dc0316f55dc3920e870f35
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-27 23:52:44 +0530

    to find Positive negative number

---
+++ src/assignments/PositiveNumber.java
@@ -0,0 +1,17 @@
+package assignments;
+
+public class PositiveNumber {
+	public static void main(String[] args) {
+		int i = -10;
+		if(i<0) {
+		
+		System.out.println("Number is negative");
+		}
+		else {
+		
+			System.out.println("Number is positive");
+		}
+			
+	}
+
+}
