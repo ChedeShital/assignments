ad8d662e180726c6dc19e6d9257ae551d7fad75b
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-28 22:30:30 +0530

    Arithmetic operators solved 5.

---
+++ src/assignments/ArithmetocOperator5.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class ArithmetocOperator5 {
+	public static void main(String[] args) {
+		int i = 10;
+
+		int j = i++ + i++ + --i + ++i + ++i; 
+
+		System.out.println(i); 
+
+		System.out.println(j);
+	}
+
+}
