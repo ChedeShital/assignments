c5f00456b9d110811d256dd6b23f884c3ca175fa
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-03-28 21:58:57 +0530

    Arithmetic operator solving.

---
+++ src/assignments/ArithmeticOperator2.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class ArithmeticOperator2 {
+	public static void main(String[] args) {
+		int i = 10; 
+
+		int j = i++ + ++i; 
+
+		System.out.println(i); 
+
+		System.out.println(j);
+	}
+
+}
