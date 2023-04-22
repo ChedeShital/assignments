6db121a0e2f92b9cc95a3a3484e3c63fd60aa11a
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-04-22 16:37:29 +0530

    Cyclic Nature solving.

---
+++ src/assignments/CyclicNature1.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class CyclicNature1 {
+	public static void main(String[] args) {
+		int i = 020;
+		int j = 20;
+		int k = 0X20;
+		
+		System.out.println("Output of i =" +i);
+		System.out.println("Output of j =" +j);
+		System.out.println("Output of j =" +k);
+	}
+
+}
