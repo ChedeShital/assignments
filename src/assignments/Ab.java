db061ee33261532bae9aaa9e18b83d6df8d2ce54
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-04-29 22:06:48 +0530

    Object creation.

---
+++ src/assignments/Ab.java
@@ -0,0 +1,29 @@
+package assignments;
+
+public class Ab {
+
+int x = 10;
+
+int y = 15;
+
+int z = 25;
+
+public static void main(String...args)
+
+{
+
+Ab test = new Ab();
+
+Ab test2 = new Ab();
+
+Ab test3 = new Ab();
+
+System.out.println(test.x);
+
+System.out.println(test2.y);
+
+System.out.println(test2.z);
+
+}
+
+}
