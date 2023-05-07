89c0bab023517f5cb0f3781549f9aa38fe733264
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-05-07 20:18:45 +0530

    Create class in java

---
+++ src/firstCodejava/B.java
@@ -0,0 +1,13 @@
+package firstCodejava;
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println(" This is public class ");
+	}
+class C{
+	
+	public static void main(String[] args) {
+		System.out.println("This is without public class");
+	}
+}
+}
