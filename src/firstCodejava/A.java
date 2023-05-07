e272e4d710cba1e0f3c12ca03bd43e9fea822a83
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-05-07 19:25:32 +0530

    Multiple classes creation.

---
+++ src/firstCodejava/A.java
@@ -0,0 +1,17 @@
+package firstCodejava;
+
+public class A {
+	
+	public static void main(String[] args) {
+		System.out.println(" This is my first class");
+		
+	}
+
+	class B{
+		
+		 public static void main(String[] args) {
+			
+			 System.out.println(" This is second class");
+		}
+	}
+}
