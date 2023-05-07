9448b3682d3f8ebbaf549e907c3bc4344e9adc86
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-05-07 22:41:50 +0530

    Cyclic nature 1

---
+++ src/firstCodejava/CyclicNature1.java
@@ -0,0 +1,20 @@
+package firstCodejava;
+
+public class CyclicNature1 {
+	public static void main(String[] args) {
+	
+		byte b1 = (byte)250;
+		byte b2 = (byte)120;
+		byte b3 = (byte)130;
+		byte b4 = (byte)300;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		System.out.println(b4);
+		
+	}
+	
+	
+	
+}
