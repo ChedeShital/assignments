ef7cd193bf5f4e0f3bb702a693340c298fe8c4d7
Pradip <Pradip@DESKTOP-4BGFAU7>
2023-05-08 20:10:37 +0530

    Unary operator solve.

---
+++ src/firstCodejava/UnaryOperator.java
@@ -0,0 +1,23 @@
+package firstCodejava;
+
+public class UnaryOperator {
+	public static void main(String[] args) {
+		
+		int i = 10;
+
+		i = i++;
+
+		System.out.println(i);
+
+
+
+
+
+		int j = 10;
+
+		j = j++ + j;
+
+		System.out.println(j);
+	}
+
+}
